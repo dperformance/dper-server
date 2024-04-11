903d44a48e95d5ed2f0e84a950b0757d12dbca24
dperformance <docyoungs@naver.com>
2024-04-11 16:55:24 +0900

    first commit

---
+++ src/main/java/com/dyson/dperserver/DperServerApplication.java
@@ -0,0 +1,13 @@
+package com.dyson.dperserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DperServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DperServerApplication.class, args);
+	}
+
+}
